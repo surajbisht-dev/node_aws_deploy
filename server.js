4968a3a1b20ea1047d1f65ce22b09cc4ea013640
SurajBisht-Nisheservice <suraj.bisht@docnish.com>
2025-09-15 11:20:25 +0530

    new api added

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get("/api/get", (req, res) => {
   res.send({ message: "Nodejs AWS Deployment" });
 });
 
+app.get("/api/get/user", (req, res) => {
+  res.send({
+    name: "Suraj Bisht",
+    email: "surajbisht1906@hmail.com",
+    role: "Developer + DevOps",
+  });
+});
+
 app.listen(9000, (req, res) => {
   console.log("Server running on port 9000");
 });
